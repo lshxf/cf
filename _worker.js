0f85fdab3d1aef3472262f6b9fcd06655d93bf45
lshxf <66930825+lshxf@users.noreply.github.com>
2023-07-04 15:45:27 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,11 +4,11 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'eeccaa38-3869-41bd-bf62-177f3ae14fab';
 
-let proxyIP = 'cdn.xn--b6gac.eu.org';
+let proxyIP = '';
 
-let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
+let dohURL = 'https://cloudflare-dns.com/dns-query'; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
 
 // v2board api environment variables
 let nodeId = ''; // 1
